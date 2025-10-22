e15c01bc0d469d289bf277faa8d3119f4daa06f9
KaeHyun <sally980330@naver.com>
2025-10-22 17:08:53 +0900

    [fix]

--- predict_result/evaluate-rpo-rag.sh
+++ predict_result/evaluate-rpo-rag.sh
@@ -13,18 +13,14 @@ time {
 for DATA_NAME in $DATASET_LIST; do
     for N_BEAM in $BEAM_LIST; do
         RULE_PATH=/share0/khyun33/myExp/infer-cwq/test_simple_add_path_GCR_acc.jsonl
-        RULE_PATH_G1=llm/results/gnn/${DATA_NAME}/rearev-sbert/_test.info
-        RULE_PATH_G2=None #llm/results/gnn/${DATA_NAME}/rearev-lmsr/test.info
 
-        python -u src/qa_prediction/predict_answer.py \
+        python -u src/predict_answer.py \
             --model_name ${MODEL_NAME} \
             -d ${DATA_NAME} \
             --prompt_path ${PROMPT_PATH} \
             --add_rule \
             --rule_path ${RULE_PATH} \
-            --rule_path_g1 ${RULE_PATH_G1} \
-            --rule_path_g2 ${RULE_PATH_G2} \
-            --model_path /share0/yjko1853/myExp/[STEP2]friendly-sft/JOINT/final/cwq-noise-no-literal/final_step_webqsp/7B-SIMPO-CWQ-WEBQSP/lora_model \
+            --model_path /scratch/khyun33/7B-SIMPO-CWQ-WEBQSP/lora_model \
             --predict_path results/KGQA-GNN-RAG-RA/rearev-sbert
             
     done
